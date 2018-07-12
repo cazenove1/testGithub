67a87088f2c6135e307448ab3eabeca9a09b2ab0
HIAPAD <Cazen_java@163.cm>
2018-07-12 09:45:14 +0800

    Initial commit

---
+++ src/main/java/com/buyduoduo/TestgithubApplication.java
@@ -0,0 +1,12 @@
+package com.buyduoduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestgithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestgithubApplication.class, args);
+	}
+}
